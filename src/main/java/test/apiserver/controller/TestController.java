0bd088e7f894ecf7d8255080ddcdb6e63b3104ec
hyeonsoo-park <hyeonsoo-park@navercorps.com>
2022-08-13 22:27:14 +0900

    change

--- src/main/java/test/apiserver/controller/TestController.java
+++ src/main/java/test/apiserver/controller/TestController.java
@@ -2,22 +2,22 @@ package test.apiserver.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-//import test.apiserver.domain.Routing;
-//import test.apiserver.repository.TestRepository;
+import test.apiserver.domain.Routing;
+import test.apiserver.repository.TestRepository;
 
 import java.util.List;
 
 @RestController
 public class TestController {
 
-//    private final TestRepository testRepository;
-//
-//    public TestController(TestRepository testRepository) {
-//        this.testRepository = testRepository;
-//    }
+    private final TestRepository testRepository;
+
+    public TestController(TestRepository testRepository) {
+        this.testRepository = testRepository;
+    }
 
     @GetMapping("/test")
-    public String test() {
-        return "HI";
+    public List<Routing> test() throws Exception{
+        return testRepository.findAll();
     }
 }
--- src/main/java/test/apiserver/domain/Routing.java
+++ src/main/java/test/apiserver/domain/Routing.java
@@ -1,19 +1,27 @@
-//package test.apiserver.domain;
-//
-//import lombok.Getter;
-//import lombok.RequiredArgsConstructor;
-//import lombok.Setter;
-//import org.springframework.data.annotation.Id;
-//
-//import java.util.Map;
-//
-//@Getter
-//@Setter
-//@RequiredArgsConstructor
-//public class Routing {
-//
-//    @Id
-//    String id;
-//    Map<String, Object> client;
-//    Map<String, Object> backend;
-//}
+package test.apiserver.domain;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+import java.util.Map;
+
+@Getter
+@Setter
+@RequiredArgsConstructor
+@Document(value="routings")
+public class Routing {
+    @Id
+    private String id;
+    private String path;
+    private String method;
+    private String domain;
+    private int rateLimit;
+    private String baseUrl;
+    private Map<String, List<Map<String, Object>>> query;
+    private Map<String, List<Map<String, Object>>> body;
+    private List<Map<String, Object>> pathVariable;
+}
