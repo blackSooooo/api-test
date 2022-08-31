f1ff6940abf3734ec86142ff955bb29f945bf7a5
hyeonsoo-park <hyeonsoo-park@navercorps.com>
2022-08-31 22:37:48 +0900

    traffic test setting

--- src/main/java/test/apiserver/controller/TestController.java
+++ src/main/java/test/apiserver/controller/TestController.java
@@ -1,24 +1,30 @@
 package test.apiserver.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import test.apiserver.domain.Routing;
 import test.apiserver.repository.TestRepository;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class TestController {
 
     private final TestRepository testRepository;
+    private final static Logger logger = LoggerFactory.getLogger(TestController.class);
 
     public TestController(TestRepository testRepository) {
         this.testRepository = testRepository;
     }
 
     @GetMapping("/test")
-    public List<Routing> test() {
-        return testRepository.findAll();
+    public List<String> test() {
+        logger.info("traffic test");
+        return Arrays.asList("ZZ");
+//        return testRepository.findAll();
     }
 
 }
